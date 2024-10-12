f1ef905745959aac743a30669258d71209593569
Juan Pablo Espinoza <juanespinoza830@pm.me>
2024-10-12 14:51:44 -0300

    work on graph

--- src/app.py
+++ src/app.py
@@ -1,16 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import pandas
 import geopandas
 import sqlite3
 import json
 from shapely import wkb
 import folium
+import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 app = Flask(__name__)
 
 # start database connection
 conn = sqlite3.connect("db.sqlite3")
 cursor = conn.cursor()
+
+# months
+months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+]
 
 @app.route("/")
 def index():
@@ -48,8 +67,11 @@ def map_home():
     return render_template("map.html", provinces=provinces, map=rendered_map)
 
 @app.route("/map_index_reports/<province>/")
-def map_index_reports(province):
-    # return list of reports for the selected province
+def map_index_reports(province: str):
+    """
+    returns a list of all the valid reports for the selected province
+    """
+
     cursor.execute("""
     select report.name, report.description
     from report
@@ -70,8 +92,10 @@ def map_index_reports(province):
     return data
 
 @app.route("/map_index_years/<province>/<report>/")
-def map_index_years(province, report):
-    # return list of years for the selected report and province
+def map_index_years(province: str, report: str):
+    """
+    return list of years for the selected report and province
+    """
     cursor.execute("""
     select data.year
     from data
@@ -90,7 +114,7 @@ def map_index_years(province, report):
     return json.dumps({"years": years})
 
 @app.route("/map/<province>/<report>/<year>/")
-def map(report, year, province):
+def map(report: str, year: int, province: str):
     """
     Called from map.html, used to generate the map which is sent dynamically to the client
     """
@@ -119,3 +143,55 @@ def map(report, year, province):
     figure.add_child(map)
 
     return figure._repr_html_()
+
+@app.route("/graph_index/")
+def graph_home():
+    """
+    called from graph.html, returns an index of all valid reports
+    """
+
+    cursor.execute("""
+    select name from report where name like '%Month%';
+    """)
+
+    reports = [{"name": row[0]} for row in cursor.fetchall()]
+
+    return render_template('graph.html', reports=reports)
+
+@app.route('/graph_per_month/<report>/<province>/<year>/')
+def graph_per_month(report: str, province: str, year: int):
+    cursor.execute("""
+        select cohort, sum(data.value)
+        from data
+        join report on data.report_id = report.id
+        join commune on data.commune_id = commune.id
+        where
+            report.name = ? AND
+            commune.province = ? and
+            data.year = ?
+        group by cohort;
+    """, (report, province, year))
+    data = cursor.fetchall()
+
+    data = pandas.DataFrame(data, columns=["cohort", "value"])
+
+    # sort by month
+    data['cohort'] = pandas.Categorical(data['cohort'], categories=months, ordered=True)
+    data = data.sort_values('cohort')
+
+    # set cohort to index
+    data = data.set_index('cohort')
+
+    # make storage in memory
+    output = io.BytesIO()
+
+    # plot data into png
+    fig = Figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    ax.title.set_text(f"Consultas por mes en {province}")
+    ax.axes.set_xlabel("Mes") # type: ignore
+    ax.axes.set_ylabel("Consultas") # type: ignore
+    data.plot(kind='bar', ax=ax)
+    FigureCanvas(fig).print_png(output)
+
+    return output.getvalue()
